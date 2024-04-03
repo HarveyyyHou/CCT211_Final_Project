968f8ba13dfb87c3be05f1333de7bdf7fcb793f0
HarveyyyHou <156716791+HarveyyyHou@users.noreply.github.com>
2024-04-03 19:16:54 -0400

    Update Rigged_Game_Final_v1.1.py

--- Rigged_Game_Final_v1.1.py
+++ Rigged_Game_Final_v1.1.py
@@ -2,6 +2,7 @@ from tkinter import *
 import tkinter as tk
 from tkinter.messagebox import *
 import tkinter.ttk as ttk
+import Random
 
 class Dice:
     def __init__(self):
@@ -11,11 +12,66 @@ class Dice:
         pass
 class Random_Number:
 
+    "Randomly selects a Number"
+
     def __init__(self):
-        pass
+        self.rigged = None
+
+        self.Spin_Button = Button(mastertk, text='Spin', activebackground="blue",
+                                 activeforeground="blue", command=self.spin)
+        self.Spin_Button.place(anchor=CENTER,relx=0.7,rely=0.4)
+
+        self.Rigged = Button(mastertk, text="Settings",
+                           command=self.rigged)
+        self.Rigged.place(anchor=CENTER,relx=0.9,rely=0.9)
+
+        self.Save = Button(mastertk, text="Save to History",
+                           command=self.history)
+        self.Save.place(anchor=CENTER,relx=0.5,rely=0.8)
+
+        self.ChosenLabel = Label(mastertk, text="Chosen Number")
+        self.ChosenLabel.place(anchor=CENTER,relx=0.2,rely=0.1)
+
+        self.helplabel = Label(mastertk, text="to")
+        self.helplabel.place(anchor=CENTER, relx=0.7, rely=0.3)
+        self.ResultFrame = Frame(mastertk, width=125, height=125, relief='raised', borderwidth=5)
+        self.ResultFrame.place(x=0, y=50)
+
+        self.RangeLabel = Label(mastertk, text="Select integer range")
+        self.RangeLabel.place(anchor=CENTER,relx=0.7,rely=0.1)
+        self.validation = mastertk.register(self.number_or_no)
+        self.front = tk.Entry(mastertk, validate="key", validatecommand=(self.validation, '%P'), width=5)
+        self.front.place(anchor=CENTER,relx=0.55,rely=0.3)
+        self.end = tk.Entry(mastertk, validate="key", validatecommand=(self.validation, '%P'), width=5)
+        self.end.place(anchor=CENTER,relx=0.85,rely=0.3)
+
 
     def spin(self):
+        if self.rigged == None:
+            frontnum = int(self.front.get())
+            endnum = int(self.end.get())
+            MyNum = str(random.randint(frontnum,endnum))
+            Numberlabel = Label(self.ResultFrame, text=MyNum)
+            Numberlabel.place(relx=0.5, rely=0.5, anchor=CENTER)
+        else:
+            Mylabel = Label(mastertk, text=self.rigged)
+            Mylabel.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+    def number_or_no(self,myint):
+        if myint.isnumeric():
+            return True
+        # Allows backspace to first character
+        elif myint == "":
+            return True
+        else:
+            return False
+
+    def rigged(self):
         pass
+    def history(self):
+        print("not implemented yet")
+        pass
+
 
 class Name_Pick:
 
@@ -41,7 +97,8 @@ current_screen = 0
 def switch_screen(screen):
     global current_screen
     current_screen = screen
-    # print(current_screen)
+    if current_screen==1:
+        my_random = Random_Number()
 
 mastertk = Tk()
 
